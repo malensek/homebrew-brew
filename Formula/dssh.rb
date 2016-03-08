6a8e153fa354328a078d39ef5d6d6e774ce17cc0
Matthew Malensek <matt@malensek.net>
2016-03-07 23:34:57 -0700

    Set prefix properly

--- Formula/dssh.rb
+++ Formula/dssh.rb
@@ -5,6 +5,7 @@ class Dssh < Formula
   sha256 "8f8eb97d7220202107ce905d33925d53a696a8bcd555b5f71a15a91099a8c302"
 
   def install
+    ENV["PREFIX"] = "#{prefix}"
     system "make", "install"
   end
 end
