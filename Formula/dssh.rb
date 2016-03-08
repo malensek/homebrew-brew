909577180968d129b9091664c20a52d909cdeba8
Matthew Malensek <matt@malensek.net>
2016-03-07 23:28:07 -0700

    Add brew formula

---
+++ Formula/dssh.rb
@@ -0,0 +1,10 @@
+class Dssh < Formula
+  desc "SSH for clusters"
+  homepage "https://sigpipe.io/dssh"
+  url "https://sigpipe.io/dssh/releases/dssh-1.0.tar.gz"
+  sha256 "8f8eb97d7220202107ce905d33925d53a696a8bcd555b5f71a15a91099a8c302"
+
+  def install
+    system "make", "install"
+  end
+end
