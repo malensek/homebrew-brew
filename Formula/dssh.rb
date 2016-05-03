55447cda8bd3bbb487e141289eb6b488eeeb095e
Matthew Malensek <matt@malensek.net>
2016-05-03 16:24:09 -0600

    Update for dssh 1.1

--- Formula/dssh.rb
+++ Formula/dssh.rb
@@ -1,8 +1,8 @@
 class Dssh < Formula
   desc "SSH for clusters"
   homepage "https://sigpipe.io/dssh"
-  url "https://sigpipe.io/dssh/releases/dssh-1.0.tar.gz"
-  sha256 "8f8eb97d7220202107ce905d33925d53a696a8bcd555b5f71a15a91099a8c302"
+  url "https://sigpipe.io/dssh/releases/dssh-1.1.tar.gz"
+  sha256 "096c9bce97778e24bb91a981bea8736e3fc279c2200fb9c0dcc8a4b54e6a2b19"
 
   def install
     ENV["PREFIX"] = "#{prefix}"
